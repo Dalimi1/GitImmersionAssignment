49d11b04cf7301f57dfd62a89d07a06f6104f72e
Dalimi1 <dalimi@uncc.edu>
2020-11-02 19:40:44 -0500

    This is the commit for the video.

--- GitImmersion/src/test.java
+++ GitImmersion/src/test.java
@@ -1,7 +1,7 @@
-public class GitImmersionTest {
+public class test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("Hello, world!");
 
 	}
 
